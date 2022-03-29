fbebc9ce833f7e0b18b3ed011edd1c5def30cd21
Prg-maker <df3296709@gmail.com>
2022-03-29 12:23:47 -0300

    add test

---
+++ frontend/src/pages/components/Task/Task.test.js
@@ -0,0 +1,15 @@
+import {render , screen} from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import {Task} from './index'
+
+describe("list Task", ()=> {
+
+  it('sums numbers', () => {
+    const {getByText} = render(<Task/>)
+    
+
+
+   
+  });
+  
+})
